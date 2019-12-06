b55c4dc13091b23a3830796f47bc1e4dcfeaf4ec
hunters <2397416572@qq.com>
2019-12-06 15:10:51 +0800

    归并排序

--- src/structure/sort/QuickSort.java
+++ src/structure/sort/QuickSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class QuickSort {
     public static void main(String[] args) {
         int arr[] = {4, 5, 2, 1, 3, 8, 7};
-        quickSort1(arr, 0, arr.length - 1);
+        quickSort2(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -76,9 +76,31 @@ public class QuickSort {
             }
             arr[low] = index;
 
-            quickSort1(arr, start, low-1);
+            quickSort1(arr, start, low - 1);
             quickSort1(arr, low + 1, end);
         }
     }
+
+    static void quickSort2(int arr[], int start, int end) {
+        if (start < end) {
+            int index = arr[start];
+            int low = start;
+            int high = end;
+            while (low < high) {
+                while (index < arr[high]&&low<high) {
+                    high--;
+                }
+                arr[low] = arr[high];
+                while (index > arr[low]&&low<high) {
+                    low++;
+                }
+                arr[high] = arr[low];
+            }
+            arr[low] = index;
+            quickSort2(arr, start, low);
+            quickSort2(arr, low + 1, end);
+        }
+    }
+
 
 }
